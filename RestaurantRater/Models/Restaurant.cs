63b15a03e20f674fcf1a8351fec52db3445b64db
jhemphill1389 <ks.jah.1776@gmail.com>
2020-11-11 08:37:14 -0600

    changedb

--- RestaurantRater/Models/Restaurant.cs
+++ RestaurantRater/Models/Restaurant.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace RestaurantRater.Models
 {
-    public class Class1
+    public class Restaurant
     {
+        public int ResaurantID { get; set; }
+            public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+
+    }
+    public class ResaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
     }
 }
